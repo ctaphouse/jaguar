5a664ae642e398fe34ba3cc6f49ddce64b076cb4
Green Hornet <taphousecarl@gmail.com>
2022-04-07 14:27:28 -0400

    add entities and first endpoint

---
+++ jaguar.api/Features/ManageStudents/Class.cs
@@ -0,0 +1,9 @@
+namespace jaguar.api.Features.ManageStudents;
+
+public class Class
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public IEnumerable<Student> Students { get; set; } = new List<Student>();
+}
